2c221b1d33abee43051f8f615ffa7928aeebe3e7
elieli22 <elidehaas@gmail.com>
2017-06-07 23:16:33 +0200

    2

---
+++ script.js
@@ -0,0 +1,10 @@
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script type="text/javascript" language="JavaScript">
+  function set_body_height() { // set body height = window height
+    $('body').height($(window).height());
+  }
+  $(document).ready(function() {
+    $(window).bind('resize', set_body_height);
+    set_body_height();
+  });
+</script>
